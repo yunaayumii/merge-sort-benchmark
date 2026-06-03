b366a6c69043c008383cdb0a539784f2e4825fc8
Yuan Miguel S. Rubio <yuanmiguelrubio@gmail.com>
2026-06-03 11:30:10 +0800

    timer decorator implementation

--- benchmarker.py
+++ benchmarker.py
@@ -3,12 +3,21 @@ from merge_sort import merge_sort
 
 def benchmark_timer(func):
   # TODO: Implement the wrapper to measure time
+  def wrapper(*args, **kwargs):
+    start_time = time.perf_counter()
+
+    result = func(*args, **kwargs)
+
+    end_time = time.perf_counter()
+    print(f"Elapsed: {end_time - start_time: 6f}s")
+    return result
+  return wrapper
   
 
 @benchmark_timer
 def run_benchmark(data):
   # TODO: Call merge_sort and return result
-  pass
+  return merge_sort(data)
 
 if __name__ == "__main__":
   # Create a list of 10,000 integers to test
