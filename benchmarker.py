762a5fa93eaf0f2fe24fcbf34b6ba25264dc47c6
Yuan Miguel S. Rubio <yuanmiguelrubio@gmail.com>
2026-06-03 11:22:36 +0800

    clean before changing

--- benchmarker.py
+++ benchmarker.py
@@ -2,15 +2,15 @@ import time
 from merge_sort import merge_sort
 
 def benchmark_timer(func):
-    # TODO: Implement the wrapper to measure time
-    pass
+  # TODO: Implement the wrapper to measure time
+  
 
 @benchmark_timer
 def run_benchmark(data):
-    # TODO: Call merge_sort and return result
-    pass
+  # TODO: Call merge_sort and return result
+  pass
 
 if __name__ == "__main__":
-    # Create a list of 10,000 integers to test
-    large_data = list(range(10000, 0, -1)) 
-    run_benchmark(large_data)
+  # Create a list of 10,000 integers to test
+  large_data = list(range(10000, 0, -1)) 
+  run_benchmark(large_data)
