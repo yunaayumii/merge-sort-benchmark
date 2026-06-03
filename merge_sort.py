bb0912725e976aeec648a0b262d752d14bfc96ee
Yuan Miguel S. Rubio <yuanmiguelrubio@gmail.com>
2026-06-03 11:11:43 +0800

    helper function

--- merge_sort.py
+++ merge_sort.py
@@ -1,21 +1,32 @@
-def merge_sort(data):
-    # Base Case: If the list is 1 or 0 elements, it's already sorted
-    if len(data) == 0 or len(data) == 1: 
-        return data
+def merge(left: list, right: list):
+	merged = []
+	# todo: while loop to compare elements
+	while len(left) > 0 and len(right) > 0: 
+		if left[0] <= right[0]:
+			merged.append(left.pop(0))
+		else:
+			merged.append(right.pop(0))
+	return merged
+		
+	# todo: handle remainig elements in left or right
+	merged.extend(left)
+	merged.extend(right)
 
-    # Divide: Find the midpoint and split the data
-    mid = len(data) // 2
-    left_half = data[:mid]
-    right_half = data[mid:]
+def merge_sort(data: list):
+	if len(data) <= 1:
+		return data
 
-    # Recursive calls
-    left_sorted = merge_sort(left_half)
-    right_sorted = merge_sort(right_half)
+	mid = len(data) // 2
+	left_half = data[:mid]
+	right_half = data[mid:]
 
-    # Temporary return for this lesson
-    return left_sorted, right_sorted
+	left_sorted = merge_sort(left_half)
+	right_sorted = merge_sort(right_half)
+
+	#replace with merge() helper
+	return merge(left_sorted, right_sorted)
 
 # Test your logic
 if __name__ == "__main__":
-    test_list = [5, 2, 9, 1]
-    print(merge_sort(test_list))
+	test_list = [5, 2, 9, 1]
+	print(merge_sort(test_list))
